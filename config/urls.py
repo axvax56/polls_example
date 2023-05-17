ec215575b9b744a41fa8fd8bef443c3a698f7580
axvax56 <axvax56@naver.com>
2023-05-17 15:17:30 +0900

    오타 수정

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from polls import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('polls/', include('polls.urls'))
+    path('polls/', include('polls.urls')),
     path("polls/", views.index, name='index'),
     path('polls/<int:question_id>/', views.detail, name= 'detail'),
     path('polls/<int:question_id>/vote/', views.vote, name ='vote'),

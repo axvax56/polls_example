e5872dba21d8145f3ad54962e247c9596f1315b7
axvax56 <axvax56@naver.com>
2023-05-17 15:03:46 +0900

    코딩 빠진거

--- config/urls.py
+++ config/urls.py
@@ -23,9 +23,8 @@ from polls import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('polls/', include('polls.urls'))
-
-    #path("polls/", views.index, name='index'),
-    #path('polls/<int:question_id>/', views.detail, name= 'detail'),
-    #path('polls/<int:question_id>/vote/', views.vote, name ='vote'),
-    #path('polls/<int:question_id>/results/', views.results, name = 'results')
+    path("polls/", views.index, name='index'),
+    path('polls/<int:question_id>/', views.detail, name= 'detail'),
+    path('polls/<int:question_id>/vote/', views.vote, name ='vote'),
+    path('polls/<int:question_id>/results/', views.results, name = 'results')
 ]

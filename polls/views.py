bb41041a7689283eb0a49a6a4eb53a488e230c6b
axvax56 <axvax56@naver.com>
2023-05-17 14:48:48 +0900

    +vote and results

--- polls/views.py
+++ polls/views.py
@@ -14,3 +14,19 @@ def index(request):
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question' : question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk =question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['chocie'])
+    except(KeyError, Chocie.DoesNotExist):
+        return render(request, 'polls/detail.html', {'question' : question, 'error_message' : "You didn't select a choice."})
+
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
+
+def results(request, question_id):
+    question = get_object_or_404(Qestion, pk=question_id)
+    return render(request, 'polls/results.html', {'question' : question})

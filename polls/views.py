ecd48a3a84e48a8508d1e0e3df5de5df87af872f
axvax56 <axvax56@naver.com>
2023-05-17 15:26:25 +0900

    tlqkf

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from polls.models import Question, Choice
 from django.http import HttpResponseRedirect
 from django.urls import reverse

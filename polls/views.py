f054a40244710137410198bae1cad29324ec8591
axvax56 <axvax56@naver.com>
2023-05-17 17:44:27 +0900

    urls.py에서 vote에 관한 주석처리 해제

--- polls/views.py
+++ polls/views.py
@@ -17,7 +17,7 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 
-def vote(request, question_id):
+def vote(request: object, question_id: object) -> object:
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])

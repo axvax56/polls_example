0adce587f37da33658ab1858db9a2e0ccd909c87
axvax56 <axvax56@naver.com>
2023-05-17 15:34:46 +0900

    tlqkf

--- polls/views.py
+++ polls/views.py
@@ -20,7 +20,7 @@ def detail(request, question_id):
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['chocie'])
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except(KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html',
                       {'question': question, 'error_message': "You didn't select a choice."})

3601159cdbadb956ca38d1f7e6c8d352181cb154
axvax56 <axvax56@naver.com>
2023-05-22 14:47:07 +0900

    필드 접기 polls.admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from polls.models import Question, Choice
 # Register your models here.
 
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [('Question Statement', {'field' : ['question_text']}),
+                 ('Date Information', {'fields' : ['pub_date'], 'classes':['collapse']})]
